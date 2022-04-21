e1ed6cbfc2ab1dc2eeaf2934fddaf23c8ccdc000
SriRajKarthik <101410708+SriRajKarthik@users.noreply.github.com>
2022-04-21 20:14:01 +0530

    Delete models directory

--- models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const url='mongodb+srv://newuser:12345@cluster0.xk3sw.mongodb.net/EmployeeDB?retryWrites=true&w=majority';
-mongoose.connect(url, { useNewUrlParser: true,useUnifiedTopology:true }, (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-
-require('./employee.model');
